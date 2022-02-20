2fb660ee96cd5c5945349496eac1f3c0f96dc9f5
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-02-20 19:49:15 +1100

    Update script.js with function that adds books in myLibrary to DOM as DOM elements.

---
+++ script.js
@@ -0,0 +1,58 @@
+//DOM Elements.
+const main = document.querySelector('main');
+let cards = [];
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) 
+{
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary()
+{
+
+}
+
+function displayBooks()
+{
+    myLibrary.forEach(book =>
+    {
+        console.log(book);
+        const newCard = document.createElement('div');
+        newCard.classList.add('card');
+
+        const title = document.createElement('h3');
+        const authorHeader = document.createElement('h4');
+        const author = document.createElement('p');
+        const pagesHeader = document.createElement('h4');
+        const pages = document.createElement('p');
+        const read = document.createElement('p');
+
+        title.textContent = book.title;
+        authorHeader.textContent = 'Author';
+        author.textContent = book.author;
+        pagesHeader.textContent = 'Number of Pages';
+        pages.textContent = book.pages;
+        pages.textContent = book.read ? 'This book has been read' : 'This book has not been read';
+
+        newCard.appendChild(title);
+        newCard.appendChild(authorHeader);
+        newCard.appendChild(author);
+        newCard.appendChild(pagesHeader);
+        newCard.appendChild(pages);
+        newCard.appendChild(read);
+
+        main.appendChild(newCard);
+        cards.push(newCard);
+    });
+}
+
+myLibrary.push(new Book("The Hobbit", "J.R.R. Tolkien", 310, false));
+
+console.table(myLibrary)
+
+displayBooks();
